9a709a54a136f6174c14dd627aa07dc6aa16f929
howelljian <114792721+howelljian@users.noreply.github.com>
2023-11-14 21:53:21 -0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ function Start()
 {
     navigator.mediaDevices.getUserMedia({audio:true});
     classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/DAU-dno7T/model.json', modelReady);
-    navigator.vibrate(10000);
+    
+    
 }
 
 function modelReady()
@@ -32,95 +33,96 @@ function gotResults(error, results)
   else if(note =="C")
   {
     document.getElementById("canvas").style.backgroundColor = "red";
-    console.log("C");
-    document.getElementById("note").innerHTML = "C";
     navigator.vibrate(500);
+    document.getElementById("note").innerHTML = "C";
+    console.log("C");
+    
   }
   else if(note =="C#")
   {
     document.getElementById("canvas").style.backgroundColor = "tomato";
-    console.log("C#");
-    document.getElementById("note").innerHTML = "C#";
     navigator.vibrate(500);
+    document.getElementById("note").innerHTML = "C#";
+    console.log("C#");
+    
   }
 
   else if(note =="D")
   {
     document.getElementById("canvas").style.backgroundColor = "orange";
-    console.log("D");
+    navigator.vibrate(500);
     document.getElementById("note").innerHTML = "D";
-    navigator.vibrate(500)
+    console.log("D");
 
   }
   else if(note =="D#")
   {
     document.getElementById("canvas").style.backgroundColor = "lightsalmon";
-    console.log("D#");
-    document.getElementById("note").innerHTML = "D#";
     navigator.vibrate(500);
+    document.getElementById("note").innerHTML = "D#";
+    console.log("D#");
   }
   else if(note =="E")
   {
     document.getElementById("canvas").style.backgroundColor = "yellow";
-    console.log("E");
-    document.getElementById("note").innerHTML = "E";
     navigator.vibrate(500);
+    document.getElementById("note").innerHTML = "E";
+    console.log("E");
 
   }
   else if(note =="F")
   {
     document.getElementById("canvas").style.backgroundColor = "gold";
-    console.log("F");
-    document.getElementById("note").innerHTML = "F";
     navigator.vibrate(500);
+    document.getElementById("note").innerHTML = "F";
+    console.log("F");
 
   }
   else if(note =="F#")
   {
     document.getElementById("canvas").style.backgroundColor = "lime";
-    console.log("F#");
-    document.getElementById("note").innerHTML = "F#";
     navigator.vibrate(500);
+    document.getElementById("note").innerHTML = "F#";
+    console.log("F#");
 
   }
   else if(note =="G")
   {
     document.getElementById("canvas").style.backgroundColor = "greenyellow";
-    console.log("G");
-    document.getElementById("note").innerHTML = "G";
     navigator.vibrate(500);
+    document.getElementById("note").innerHTML = "G";
+    console.log("G");
 
   }
   else if(note =="G#")
   {
     document.getElementById("canvas").style.backgroundColor = "cyan";
-    console.log("G#");
-    document.getElementById("note").innerHTML = "G#";
     navigator.vibrate(500);
-
+    document.getElementById("note").innerHTML = "G#";
+    console.log("G#");
   }
   else if(note == "A")
   {
     document.getElementById("canvas").style.backgroundColor = "deepskyblue";
-    console.log("A");
-    document.getElementById("note").innerHTML = "A";
     navigator.vibrate(500);
+    document.getElementById("note").innerHTML = "A";
+    console.log("A");
   }
 
   else if(note == "A#")
   {
     document.getElementById("canvas").style.backgroundColor = "deeporchid";
-    console.log("A#");
-    document.getElementById("note").innerHTML = "A#";
     navigator.vibrate(500);
+    document.getElementById("note").innerHTML = "A#";
+    console.log("A#");
   }
 
   else if(note == "B")
   {
     document.getElementById("canvas").style.backgroundColor = "violet";
-    console.log("B");
-    document.getElementById("note").innerHTML = "B";
     navigator.vibrate(500);
+    document.getElementById("note").innerHTML = "B";
+    console.log("B");
   }
   else if(note == "Background Noise")
   {
